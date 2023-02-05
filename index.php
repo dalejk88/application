ac5e1a3fcf0ee5453bcf9e88b39d7592b47e3b06
Dale Kanikkeberg <kanikkeberg.dale@student.greenriver.edu>
2023-02-05 15:30:42 -0500

    Add directory structure - basic setup

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+// Controller file
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+// Error reporting level
+error_reporting(E_ALL);
+
+// Start a session
+session_start();
+
+// Require autoload file
+require_once('vendor/autoload.php');
+
+//Insantiate F3 Base class
+$f3 = Base::instance();
+
+// Define a default route (328/home)
+$f3->route('GET /', function(){
+    // Instantiate a view
+    $view = new Template();
+    echo $view->render("views/home.html");
+});
+
+// Run Fat Free
+$f3->run();
