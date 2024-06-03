cc9943177a2dd60440f32d02bc0a9e2a021574f8
Dale Kanikkeberg <kanikkeberg.dale@student.greenriver.edu>
2024-06-03 05:02:26 -0400

    Added Applicant_SubscribedToLists class

---
+++ classes/applicant_subscribedToLists.php
@@ -0,0 +1,45 @@
+<?php
+
+/** Represents an applying user that checks
+ * the mailing lists checkbox
+ */
+
+class Applicant_SubscribedToLists extends Applicant
+{
+    private array $_selectionsJobs;
+    private array $_selectionsVerticals;
+
+    /**
+     * @return array
+     */
+    public function getSelectionsJobs(): array
+    {
+        return $this->_selectionsJobs;
+    }
+
+    /**
+     * @param array $selectionsJobs
+     */
+    public function setSelectionsJobs(array $selectionsJobs): void
+    {
+        $this->_selectionsJobs = $selectionsJobs;
+    }
+
+    /**
+     * @return array
+     */
+    public function getSelectionsVerticals(): array
+    {
+        return $this->_selectionsVerticals;
+    }
+
+    /**
+     * @param array $selectionsVerticals
+     */
+    public function setSelectionsVerticals(array $selectionsVerticals): void
+    {
+        $this->_selectionsVerticals = $selectionsVerticals;
+    }
+
+
+}
