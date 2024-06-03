e598b7e56f84d2912e6aac20ea1101985af31ead
Dale Kanikkeberg <kanikkeberg.dale@student.greenriver.edu>
2024-06-03 01:39:00 -0400

    Moved all routes to controller file

--- controllers/controller.php
+++ controllers/controller.php
@@ -18,5 +18,137 @@ class Controller
         echo $view->render('views/home.html');
     }
 
+    function info()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            // Get the data from the POST array
+            $firstName = "";
+            $lastName = "";
+            $email = "";
+            $state = $_POST['state'];
+            $phone = "";
 
+            // If the data is valid
+            // Get the data from the post array
+
+            // First Name
+            if (Validate::validName($_POST['firstName'])) {
+                $firstName = $_POST['firstName'];
+            }
+            else {
+                $this->_f3->set('errors["firstName"]', 'Please enter a valid name');
+            }
+            // Last Name
+            if (Validate::validName($_POST['lastName'])) {
+                $lastName = $_POST['lastName'];
+            }
+            else {
+                $this->_f3->set('errors["lastName"]', 'Please enter a valid name');
+            }
+            // Email
+            if (Validate::validEmail($_POST['email'])) {
+                $email = $_POST['email'];
+            }
+            else {
+                $this->_f3->set('errors["email"]', 'Please enter a valid email');
+            }
+            // Phone
+            if (Validate::validPhone($_POST['phone'])) {
+                $phone = $_POST['phone'];
+            }
+            else {
+                $this->_f3->set('errors["phone"]', 'Please enter a valid phone number');
+            }
+
+            // Add the data to the session array
+            $this->_f3->set('SESSION.firstName', $firstName);
+            $this->_f3->set('SESSION.lastName', $lastName);
+            $this->_f3->set('SESSION.email', $email);
+            $this->_f3->set('SESSION.state', $state);
+            $this->_f3->set('SESSION.phone', $phone);
+
+            // If there are no errors,
+            // Send the user to the next form
+            if(empty($this->_f3->get('errors'))) {
+                $this->_f3->reroute('experience');
+            }
+        }
+        // Render a view page
+        $view = new Template();
+        echo $view->render('views/info.html');
+    }
+
+    function experience()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            // Get the data from the POST array
+            $biography = $_POST['biography'];
+            $githubLink = "";
+            $yearsExperience = "";
+            $relocate = $_POST['relocate'];
+
+            // If the data is valid
+            // Get the data from the post array
+
+            // GitHub
+            if (Validate::validGithub($_POST['githubLink'])) {
+                $githubLink = $_POST['githubLink'];
+            }
+            else {
+                $this->_f3->set('errors["githubLink"]', 'Please enter a valid link');
+            }
+
+            // Experience
+            if (Validate::validExperience($_POST['yearsExperience'])) {
+                $yearsExperience = $_POST['yearsExperience'];
+            }
+            else {
+                $this->_f3->set('errors["yearsExperience"]', 'Please select a valid option');
+            }
+
+            // Add the data to the session array
+            $this->_f3->set('SESSION.biography', $biography);
+            $this->_f3->set('SESSION.githubLink', $githubLink);
+            $this->_f3->set('SESSION.yearsExperience', $yearsExperience);
+            $this->_f3->set('SESSION.relocate', $relocate);
+
+            // If there are no errors,
+            // Send the user to the next form
+            if(empty($this->_f3->get('errors'))) {
+                $this->_f3->reroute('mailing');
+            }
+        }
+        // Render a view page
+        $view = new Template();
+        echo $view->render('views/experience.html');
+    }
+
+    function mailing()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            // Get the data from the POST array
+            if (isset($_POST["jobOpenings"])) {
+                $jobOpenings = implode(", ",$_POST["jobOpenings"]);
+            }
+
+            // If the data is valid
+            if (true) {
+                // Add the data to the session array
+                $this->_f3->set('SESSION.jobOpenings', $jobOpenings);
+
+                // Send the user to the next form
+                $this->_f3->reroute("summary");
+            }
+        }
+        // Render a view page
+        $view = new Template();
+        echo $view->render('views/mailing.html');
+    }
+
+    function summary()
+    {
+        // Render a view page
+        $view = new Template();
+        echo $view->render('views/summary.html');
+    }
 }
--- index.php
+++ index.php
@@ -25,143 +25,23 @@ $f3->route('GET /', function() {
 });
 
 // Define an information route
-$f3->route('GET|POST /info', function($f3) {
-    if ($_SERVER['REQUEST_METHOD'] == "POST") {
-        // Get the data from the POST array
-        $firstName = "";
-        $lastName = "";
-        $email = "";
-        $state = $_POST['state'];
-        $phone = "";
-
-        // If the data is valid
-        // Get the data from the post array
-
-        // First Name
-        if (Validate::validName($_POST['firstName'])) {
-            $firstName = $_POST['firstName'];
-        }
-        else {
-            $f3->set('errors["firstName"]', 'Please enter a valid name');
-        }
-        // Last Name
-        if (Validate::validName($_POST['lastName'])) {
-            $lastName = $_POST['lastName'];
-        }
-        else {
-            $f3->set('errors["lastName"]', 'Please enter a valid name');
-        }
-        // Email
-        if (Validate::validEmail($_POST['email'])) {
-            $email = $_POST['email'];
-        }
-        else {
-            $f3->set('errors["email"]', 'Please enter a valid email');
-        }
-        // Phone
-        if (Validate::validPhone($_POST['phone'])) {
-            $phone = $_POST['phone'];
-        }
-        else {
-            $f3->set('errors["phone"]', 'Please enter a valid phone number');
-        }
-
-        // Add the data to the session array
-        $f3->set('SESSION.firstName', $firstName);
-        $f3->set('SESSION.lastName', $lastName);
-        $f3->set('SESSION.email', $email);
-        $f3->set('SESSION.state', $state);
-        $f3->set('SESSION.phone', $phone);
-
-        // If there are no errors,
-        // Send the user to the next form
-        if(empty($f3->get('errors'))) {
-            $f3->reroute('experience');
-        }
-    }
-    // Render a view page
-    $view = new Template();
-    echo $view->render('views/info.html');
+$f3->route('GET|POST /info', function() {
+    $GLOBALS['con']->info();
 });
 
 // Define an experience route
-$f3->route('GET|POST /experience', function($f3) {
-    //var_dump ($f3->get("SESSION"));
-
-    if ($_SERVER['REQUEST_METHOD'] == "POST") {
-        // Get the data from the POST array
-        $biography = $_POST['biography'];
-        $githubLink = "";
-        $yearsExperience = "";
-        $relocate = $_POST['relocate'];
-
-        // If the data is valid
-        // Get the data from the post array
-
-        // GitHub
-        if (Validate::validGithub($_POST['githubLink'])) {
-            $githubLink = $_POST['githubLink'];
-        }
-        else {
-            $f3->set('errors["githubLink"]', 'Please enter a valid link');
-        }
-
-        // Experience
-        if (Validate::validExperience($_POST['yearsExperience'])) {
-            $yearsExperience = $_POST['yearsExperience'];
-        }
-        else {
-            $f3->set('errors["yearsExperience"]', 'Please select a valid option');
-        }
-
-        // Add the data to the session array
-        $f3->set('SESSION.biography', $biography);
-        $f3->set('SESSION.githubLink', $githubLink);
-        $f3->set('SESSION.yearsExperience', $yearsExperience);
-        $f3->set('SESSION.relocate', $relocate);
-
-        // If there are no errors,
-        // Send the user to the next form
-        if(empty($f3->get('errors'))) {
-            $f3->reroute('mailing');
-        }
-    }
-    // Render a view page
-    $view = new Template();
-    echo $view->render('views/experience.html');
+$f3->route('GET|POST /experience', function() {
+    $GLOBALS['con']->experience();
 });
 
 // Define a mailing route
-$f3->route('GET|POST /mailing', function($f3) {
-    //var_dump ($f3->get("SESSION"));
-
-    if ($_SERVER['REQUEST_METHOD'] == "POST") {
-        // Get the data from the POST array
-        if (isset($_POST["jobOpenings"])) {
-            $jobOpenings = implode(", ",$_POST["jobOpenings"]);
-        }
-
-        // If the data is valid
-        if (true) {
-            // Add the data to the session array
-            $f3->set('SESSION.jobOpenings', $jobOpenings);
-
-            // Send the user to the next form
-            $f3->reroute("summary");
-        }
-    }
-    // Render a view page
-    $view = new Template();
-    echo $view->render('views/mailing.html');
+$f3->route('GET|POST /mailing', function() {
+    $GLOBALS['con']->mailing();
 });
 
 // Define a summary route
 $f3->route('GET /summary', function($f3) {
-    //var_dump ($f3->get("SESSION"));
-
-    // Render a view page
-    $view = new Template();
-    echo $view->render('views/summary.html');
+    $GLOBALS['con']->summary();
 });
 
 // Run Fat-Free
